543694f281ff43bb834d5809195d4588914d1896
pismeno <krystof.kloz@gmail.com>
2026-03-13 09:28:03 +0100

    fixed post listing, added pagination

--- functions.php
+++ functions.php
@@ -9,44 +9,49 @@ function ob_action(string $action) : string
 
 function replace_keys(string $viewFilename) : string
 {
-    $GLOBAL_KEYS = [
-        'WP_HEAD' => ob_action('wp_head'),
-        'WP_FOOTER' => ob_action('wp_footer')
-    ];
+    $main_view = file_get_contents(get_template_directory() . '/views/' . $viewFilename);
 
-    $view = file_get_contents(get_template_directory() . '/views/' . $viewFilename);
+    $post_template = file_get_contents(get_template_directory() . '/views/one_post.html');
 
-    foreach ($GLOBAL_KEYS as $key => $value) {
-        $view = str_replace("{{ " . $key . " }}", $value, $view);
-    }
+    $all_posts = "";
 
-    // TODO
-    if (!have_posts()) $view = $view;
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
 
-    while (have_posts()) {
-        the_post();
+            $current_post_html = $post_template;
 
-        $POST_KEYS = [
-            'CLASS' => in_category('3') ? 'post-cat-three' : 'post',
-            'TITLE' => get_the_title(),
-            'TITLE_ATTRIBUTE' => the_title_attribute(['echo' => false]),
-            'PERMALINK' => get_permalink(),
-            'THUMBNAIL' => get_the_post_thumbnail(),
-            'CONTENT' => get_the_content(),
-            'AUTHOR' => get_the_author(),
-            'DATE' => get_the_date(),
-            'TIME' => get_the_time('F jS, Y'),
-            'AUTHOR_POSTS_LINK' => get_the_author_posts_link(),
-            'CATEGORIES' => get_the_category_list(', ')
-        ];
+            $POST_KEYS = [
+                'CLASS' => in_category('3') ? 'post-cat-three' : 'post',
+                'TITLE' => get_the_title(),
+                'PERMALINK' => get_permalink(),
+                'THUMBNAIL' => get_the_post_thumbnail(),
+                'CONTENT' => get_the_content(),
+                'TIME' => get_the_time('F jS, Y'),
+                'AUTHOR_POSTS_LINK' => get_the_author_posts_link(),
+                'CATEGORIES' => get_the_category_list(', ')
+            ];
 
+            foreach ($POST_KEYS as $key => $value) {
+                $current_post_html = str_replace("{{ " . $key . " }}", $value, $current_post_html);
+            }
 
-        foreach ($POST_KEYS as $key => $value) {
-            $view = str_replace("{{ " . $key . " }}", $value, $view);
+            $all_posts .= $current_post_html;
         }
     }
 
-    return $view;
+    $GLOBAL_KEYS = [
+        'WP_HEAD' => ob_action('wp_head'),
+        'WP_FOOTER' => ob_action('wp_footer'),
+        'PAGINATION' => get_the_posts_pagination(),
+        'POST_LOOP' => $all_posts
+    ];
+
+    foreach ($GLOBAL_KEYS as $key => $value) {
+        $main_view = str_replace("{{ " . $key . " }}", (string)$value, $main_view);
+    }
+
+    return $main_view;
 }
 function enqueue_styles() {
     wp_enqueue_style(
@@ -58,5 +63,4 @@ function enqueue_styles() {
     );
 }
 
-//add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
 add_theme_support( 'post-thumbnails' );
